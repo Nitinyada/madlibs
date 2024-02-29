01bcc861a94a9dd520833ac1335677a947bcc0c4
Nitinyada <94897177+Nitinyada@users.noreply.github.com>
2024-02-29 10:15:25 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+noun:
+lion
+adjective: 
+king
+adverb:
+forest
+verb:
+family
+noun1:
+pride
+once upon a time, there was a lionking who  is living inforest with their familyand they are living happily with pride. 
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
